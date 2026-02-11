bf05c1361cd9cfe272b674326df012debce34bbe
Jonathan <jonathancornejox@gmail.com>
2026-02-10 23:07:16 -0800

    Initial commit

---
+++ src/main/java/com/jonathan/syncprobe/SyncProbeApplication.java
@@ -0,0 +1,13 @@
+package com.jonathan.syncprobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncProbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncProbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathan/syncprobe/SyncProbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.syncprobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncProbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
