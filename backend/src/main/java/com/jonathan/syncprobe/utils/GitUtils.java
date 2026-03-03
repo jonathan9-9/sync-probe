020ca6bdebf0b356b26fb47f3b16779ec822edc4
Jonathan <jonathancornejox@gmail.com>
2026-03-03 14:36:53 -0800

    clone repo git utils helper method using process builder

--- backend/src/main/java/com/jonathan/syncprobe/utils/GitUtils.java
+++ backend/src/main/java/com/jonathan/syncprobe/utils/GitUtils.java
@@ -7,7 +7,19 @@ import java.nio.file.Path;
  * */
 public class GitUtils {
     public static void cloneRepo(Path targetDir, String repoUrl) {
-
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(
+                    "git", "clone", repoUrl, targetDir.toString()
+            );
+            processBuilder.inheritIO();
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+            if (exitCode != 0){
+                throw new RuntimeException("Git clone failed");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Error cloning repository", e);
+        }
     }
     public static void pullLatest(Path repoDir){} // pull the latest changes
     public static boolean isGitRepository(Path dir){
